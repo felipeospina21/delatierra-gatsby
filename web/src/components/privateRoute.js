71d1d3ccec023484baca9d53d131013cbc76f140
felipe ospina calle <felipeospina21@hotmail.com>
2020-11-23 17:10:20 -0500

    private route with useEffect hook

--- web/src/components/privateRoute.js
+++ web/src/components/privateRoute.js
@@ -1,12 +1,15 @@
-import React from "react"
-import { navigate } from "gatsby"
-import { isLoggedIn } from "../services/auth"
+import React, { useEffect } from "react";
+import { navigate } from "gatsby";
+import { isLoggedIn } from "../services/auth";
 
 const PrivateRoute = ({ component: Component, location, ...rest }) => {
-  if (!isLoggedIn() && location.pathname !== `/app/login`) {
-    navigate("/app/login")
-    return null
-  }
-  return <Component {...rest} />
-}
-export default PrivateRoute
+  useEffect(() => {
+    if (!isLoggedIn() && location.pathname !== `/app/login`) {
+      navigate("/app/login");
+      // return null
+    }
+  }, [isLoggedIn, location]);
+
+  return isLoggedIn ? <Component {...rest} /> : null;
+};
+export default PrivateRoute;
