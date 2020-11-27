178103b9c723b9400ce4d831ed4437e75c353d63
felipe ospina calle <felipeospina21@hotmail.com>
2020-11-26 19:33:36 -0500

    sanity studio routes changed

--- studio/src/dashboardConfig.js
+++ studio/src/dashboardConfig.js
@@ -14,13 +14,13 @@ export default {
                 {
                   buildHookId: '5f77bcef218ca027bd454e5a',
                   title: 'Sanity Studio',
-                  name: 'sanity-gatsby-blog-studio-s8su11km',
+                  name: 'delatierra-studio',
                   apiId: 'b159032f-8ec2-4f2e-9ff8-7a02469d823d'
                 },
                 {
                   buildHookId: '5f77bcef68402726e1cea9ca',
                   title: 'Blog Website',
-                  name: 'sanity-gatsby-blog-web-w2sq865y',
+                  name: 'delatierra-web',
                   apiId: 'af41be47-3d2d-4ada-aaa8-4d524aedcf80'
                 }
               ]
@@ -30,10 +30,10 @@ export default {
         data: [
           {
             title: 'GitHub repo',
-            value: 'https://github.com/felipeospina21/sanity-gatsby-blog',
+            value: 'https://github.com/felipeospina21/delatierra-gatsby',
             category: 'Code'
           },
-          { title: 'Frontend', value: 'https://sanity-gatsby-blog-web-w2sq865y.netlify.app', category: 'apps' }
+          { title: 'Frontend', value: 'https://delatierra-web.netlify.app', category: 'apps' }
         ]
       }
     },
